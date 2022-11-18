f543005b4803c4e5b4360b8a60dd5495393c2fcc
Yigal Irani <yigal@symbolclick.com>
2022-11-18 15:36:17 -0500

    changed the scene a bit

--- app.js
+++ app.js
@@ -6,18 +6,32 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+function make_cube(i){
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00+i*20 } );
+  const cube = new THREE.Mesh( geometry, material );
+  cube.position.x=i/10
+  cube.position.y=i/10
+  return cube
+}
+var N=10
+var cubes=[]
+for (let i=0;i<N;i++){
+  var cube=make_cube(i)
+  cubes.push(cube)
+  scene.add( cube );
+}
 
 camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame( animate );
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  for (let i=0;i<N;i++){
+    cubes[i].rotation.x += 0.02*i;
+    cubes[i].rotation.y += 0.02*i;
+  }
+  camera.rotation.z+=.001
+  camera.rotation.x+=.001
 
   renderer.render( scene, camera );
 }
